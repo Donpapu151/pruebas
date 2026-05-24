71986f9d113fe8f027d087fbbbf67d33aad5f229
Donpapu151 <eberardo1087@gmail.com>
2026-05-24 01:26:01 +0000

    Agregadas ofertas aleatorias y formato embed estetico

--- bot_ofertas.py
+++ bot_ofertas.py
@@ -1,11 +1,11 @@
 import requests
 import random
 
-# 🛍️ TU ENLACE DE DISCORD:
+# 🛍️ Enlace de discord aqui chaval
 URL_MI_WEBHOOK = "https://discord.com/api/webhooks/1507247515869646901/H_vZBpFd1q6wpe1oeDWo_XQKOLjgzxM2_gpdNFv0hMior6EO_owMcL3s-BkCSjldMy_D"
 
 def obtener_y_enviar_ofertas():
-    # 🌟 CAMBIO CLAVE: Cambiamos el orden a 'Recent' (Ofertas nuevas) para asegurar que la lista mute constantemente
+    
     url_mejores_ofertas = "https://www.cheapshark.com/api/1.0/deals?upperPrice=40&pageSize=150&sortBy=Recent"
     
     nombres_tiendas = {
@@ -18,26 +18,26 @@ def obtener_y_enviar_ofertas():
         if respuesta.status_code == 200:
             todas_las_ofertas = respuesta.json()
             
-            # 🔀 ¡Mezclamos la lista completa ANTES de filtrar para romper el orden fijo de la API!
+           
             random.shuffle(todas_las_ofertas)
             
             ofertas_filtradas = []
             juegos_agregados = set()
             
             for juego in todas_las_ofertas:
-                # Si ya juntamos las 5 ofertas variadas que queríamos, nos detenemos
+               
                 if len(ofertas_filtradas) >= 5:
                     break
                     
                 titulo = juego['title']
                 ahorro = round(float(juego['savings']))
                 
-                # Filtro intermedio: Que tengan al menos un 25% de descuento real
+               
                 if ahorro >= 25 and titulo not in juegos_agregados:
                     juegos_agregados.add(titulo)
                     ofertas_filtradas.append(juego)
             
-            # 🎨 Construimos las tarjetas estéticas (Embeds)
+
             embeds = []
             for juego in ofertas_filtradas:
                 titulo = juego['title']
@@ -47,7 +47,7 @@ def obtener_y_enviar_ofertas():
                 id_tienda = juego['storeID']
                 tienda = nombres_tiendas.get(id_tienda, "Otra tienda de PC")
                 
-                # Sacamos la foto del juego que nos da la API para que se vea INCREÍBLE
+                
                 imagen_juego = juego.get('thumb', '')
                 
                 embed = {
